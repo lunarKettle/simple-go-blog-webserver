dead747971982911754e3bda7f62140816d22ed6
lunarKettle <1evgeny.nesterenko1@gmail.com>
2024-07-17 22:47:43 +0300

    move error handling to a separate file

---
+++ internal/transport/error_handling.go
@@ -0,0 +1,35 @@
+package transport
+
+import (
+	"errors"
+	"log"
+	"net/http"
+	"simple-go-blog-webserver/internal/repository"
+)
+
+type Handler = func(http.ResponseWriter, *http.Request) error
+
+func ErrorHandling(handler Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if err := handler(w, r); err != nil {
+			if errors.Is(err, repository.ErrEmailIsOccupied) {
+				http.Error(w, err.Error(), http.StatusConflict)
+				log.Println(err)
+				return
+			}
+			if errors.Is(err, repository.ErrUsernameIsOccupied) {
+				http.Error(w, err.Error(), http.StatusConflict)
+				log.Println(err)
+				return
+			}
+			if errors.Is(err, repository.ErrFailToGetUsers) {
+				http.Error(w, "Failed to get users from database", http.StatusInternalServerError)
+				log.Println("Failed to get users from database")
+				return
+			}
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			log.Println(err)
+			return
+		}
+	})
+}
--- internal/transport/server.go
+++ internal/transport/server.go
@@ -2,9 +2,7 @@ package transport
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
-	"log"
 	"net/http"
 	"simple-go-blog-webserver/internal/models"
 	"simple-go-blog-webserver/internal/repository"
@@ -12,33 +10,6 @@ import (
 	"strings"
 	"time"
 )
-
-type Handler = func(http.ResponseWriter, *http.Request) error
-
-func ErrorHandling(handler Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if err := handler(w, r); err != nil {
-			if errors.Is(err, repository.ErrEmailIsOccupied) {
-				http.Error(w, err.Error(), http.StatusConflict)
-				log.Println(err)
-				return
-			}
-			if errors.Is(err, repository.ErrUsernameIsOccupied) {
-				http.Error(w, err.Error(), http.StatusConflict)
-				log.Println(err)
-				return
-			}
-			if errors.Is(err, repository.ErrFailToGetUsers) {
-				http.Error(w, "Failed to get users from database", http.StatusInternalServerError)
-				log.Println("Failed to get users from database")
-				return
-			}
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			log.Println(err)
-			return
-		}
-	})
-}
 
 type HTTPServer struct {
 	Address        string
